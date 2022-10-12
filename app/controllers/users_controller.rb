6558cff3f4283739a0c347fc1b193c623ef8363a
Ubuntu <ubuntu@ip-172-31-3-135.ec2.internal>
2022-10-12 07:36:23 +0000

    Usersコントローラ(index, new, create, destroy)作る

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    def index
+        #new
+    end
+    def new
+        @user = User.new
+    end
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to root_path
+    end
+    def create
+        @user = user.new(message: params[:user][:message], tdate: Time.current, image: image)
+        if @user.save
+            #redirect_to '/'
+            redirect_to root_path
+            #redirect_to controller: :users, action: :index #=>一覧ページにリダイレクトする
+        else
+            render 'new'
+        end
+    end
+    def delete
+        @users = User.all
+    end
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to root_path
+    end
 end
